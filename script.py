8f0aa113f08de941be49576352875c08fc680a26
cityactivation <47616561+cityactivation@users.noreply.github.com>
2019-02-16 11:31:51 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Congratulations, you just ran a Python script from the command line\n"
+     ]
+    }
+   ],
+   "source": [
+    "'''Python 1 - DAT-119 - Spring 2019\n",
+    "Coral Sheldon-Hess\n",
+    "2/8/2019\n",
+    "Incredibly simple and somewhat boring example program for running on the command line\n",
+    "'''\n",
+    "\n",
+    "print(\"Congratulations, you just ran a Python script from the command line\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
